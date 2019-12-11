33e9a94e3fb9c55efa0ec4ed9ff7957d7cccd593
jvictor27 <jvictor451@gmail.com>
2019-12-11 15:30:07 -0300

    Conectando com o banco de dados usando TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProdutosModule } from './produtos/produtos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [ProdutosModule]
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ProdutosModule
+  ]
 })
 export class AppModule {}
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'dev',
+    password: '123456',
+    database: 'product_management',
+    entities: [__dirname + '/../**/*.entity.ts'],
+    synchronize: true
+};
